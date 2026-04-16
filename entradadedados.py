124931380506b28471ccd2634a651d307e2dedea
Felipemonteiro10 <monteirofelipep2@gmail.com>
2026-04-16 11:12:15 -0300

    atividade em sala

---
+++ entradadedados.py
@@ -0,0 +1,14 @@
+#entrada de dados
+nome = input("digite seu nome: ")
+idade = input("digites sua idade: ")
+altura = input("digite sua altura: ")
+
+print(f"\nbem vindo, {nome} \nsua idade: {idade} \nsua altura: {altura}") 
+
+n1 = int (input("digite o numero 1:"))
+n2 = int (input("digite numero 2:"))
+valor = n1 + n2
+print(f"resultado: {valor}" )
+
+pi = ("3,1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679...")
+print(f"o valor de pi é: {pi}")
