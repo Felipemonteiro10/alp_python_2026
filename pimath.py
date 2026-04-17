eefcb70c42bf7dc283efefb2954a8cac9fb51dec
Felipemonteiro10 <monteirofelipep2@gmail.com>
2026-04-17 10:33:59 -0300

    manda

--- pimath.py
+++ pimath.py
@@ -1,4 +1,6 @@
 from math import pi
+
 raio = int(input("Digite o raio da circuferência: "))
 comprimento = 2*pi*raio
+
 print(f"O comprimento da circuferência é: {comprimento:.2f}")
--- planocarte.py
+++ planocarte.py
@@ -1,8 +1,10 @@
 from math import sqrt
+
 x1 = int(input("digite o valor de x1: "))
 y1 = int(input("digite o valor de y1: "))
 x2 = int(input("digite o valor de x2: "))
 y2 = int(input("digite o valor de y2: "))
 
 d = sqrt((x2-x1)**2 + (y2-y1)**2)
+
 print(f"o valor d distancia entre o dois pontos é: {d:.2f}")
